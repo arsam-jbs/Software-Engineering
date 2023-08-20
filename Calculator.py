102304d8ef6f6fd381557e331d879955726f006f
shumail-15 <141956795+shumail-15@users.noreply.github.com>
2023-08-20 17:54:12 +0500

    Average Function
    Added an Average Function (shumail-21387)

--- Calculator.py
+++ Calculator.py
@@ -17,6 +17,10 @@ def divide(x, y):
 # This function takes y as power of x and return the result 
 def pow(x,y):
     return x**y
+    
+# This function gives average of x and y as the result
+def avg(x,y):
+    return (x+y)/2
 
 print("Select operation.")
 print("1.Add")
@@ -24,13 +28,14 @@ print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 print("5.Power")
+print("6.Average")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4/5): ")
+    choice = input("Enter choice(1/2/3/4/5/6): ")
 
     # check if choice is one of the five options
-    if choice in ('1', '2', '3', '4','5'):
+    if choice in ('1', '2', '3', '4','5','6'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -52,6 +57,9 @@ while True:
             
         elif choice == '5':
             print(num1, "^",num2,"=", pow(num1,num2))
+        
+        elif choice == '6':
+            print("Average of ", num1, "and", num2, "is ", avg(num1,num2))
         
         # check if user wants another calculation
         # break the while loop if answer is no
